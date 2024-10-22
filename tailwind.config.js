12305ef944c598a644e93cfcbc45a398b7711d1d
usernameiskheejay <galvez.kjm@optimixr.com>
2024-10-22 21:59:47 +0800

    ap

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.{html,js}"], // Adjust this to include files in the root directory
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
